bbc142e463e021ae202bd3e7e7f534c775824240
Creeperman007 <foltinmatej@gmail.com>
2017-01-15 21:32:29 +0100

    Změna hierarchie souborů

---
+++ CSharp/Nejvetsi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nejvetsi
+{
+    class Nejvetsi
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej první číslo: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Zadej druhé číslo: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Zadej třetí číslo: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double max = a;
+            if (b > max)
+            {
+                max = b;
+            }
+            if (c > max)
+            {
+                max = c;
+            }
+            Console.WriteLine("Největší číslo je: " + max);
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Debug and testing
 Nevím, proč se to jmenuje *Debug and testing,* když tu dávám kódy z programování :hushed:
 ###11. 1. 2017
-[Test.verze1.cs](Test.verze1.cs)<br>
-[Test.verze2.cs](Test.verze2.cs)
+[Test.verze1.cs](CSharp/Test.verze1.cs)<br>
+[Test.verze2.cs](CSharp/Test.verze2.cs)
 ###4. 1. 2017
-[Trojuhelnik.cs](Trojuhelnik.cs)
+[Trojuhelnik.cs](CSharp/Trojuhelnik.cs)
 ###21. 12. 2016
-[Deleni.cs](Deleni.cs)<br>
-[Nejvetsi.cs](Nejvetsi.cs)
+[Deleni.cs](CSharp/Deleni.cs)<br>
+[Nejvetsi.cs](CSharp/Nejvetsi.cs)
 ###14. 12. 2016
-[Cyklus.Sulir.cs](Cyklus.Sulir.cs)<br>
-[Cyklus.cs](Cyklus.cs)
+[Cyklus.Sulir.cs](CSharp/Cyklus.Sulir.cs)<br>
+[Cyklus.cs](CSharp/Cyklus.cs)
 ###7. 12. 2016
-[Kalkulacka.cs](Kalkulacka.cs)
+[Kalkulacka.cs](CSharp/Kalkulacka.cs)
