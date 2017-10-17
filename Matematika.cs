5304d1689c92b54bfd0c9748b8b28ad9aa405af3
Creeperman007 <foltinmatej@gmail.com>
2017-10-17 12:39:15 +0200

    17. 10. 2017

---
+++ Matematika.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Matematika
+    {
+        double parametr_a = 10;
+        double parametr_b = 5;
+
+        public string VypisLinRovnici()
+        {
+            if (parametr_b >= 0)
+                return String.Format("y = {0}x + {1}", parametr_a, parametr_b);
+            else
+                return String.Format("y = {0}x - {1}", parametr_a, -1*parametr_b);
+        }
+        public void InvertujParametry()
+        {
+            parametr_a *= -1;
+            parametr_b *= -1;
+        }
+        public void NastavParametry(double a, double b)
+        {
+            parametr_a = a;
+            parametr_b = b;
+        }
+        public bool JeSude(double cislo)
+        {
+            if (cislo % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0}, {1}", parametr_a, parametr_b);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematika pche = new Matematika();
+            Console.WriteLine(pche.VypisLinRovnici()); //výpis rovnice
+            pche.InvertujParametry(); //inverze parametrů
+            pche.NastavParametry(10, 5); //nastavení parametrů
+            Console.WriteLine(pche.JeSude(8)); //ověření, žda je číslo sudé
+            Console.WriteLine(pche.ToString()); //funkce ToString()
+
+            Console.ReadKey();
+        }
+    }
+}
